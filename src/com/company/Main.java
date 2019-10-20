64d6bcdf516b37f4e99cffd3f7cd04102d4e8b51
Nikolaj Redko <nikolajred@gmail.com>
2019-10-21 01:08:14 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    public static int count;
+
+    public static void printer(int count){
+        for (int i = 0; i <= count ; i++) {
+            System.out.println("This's simple project for GIT");
+        }
+    }
+
+    public static void main(String[] args) {
+	printer(10);
+    }
+}
